ff9f638053926b4a07955d4dbb973d7b1b97c8b4
sharma7422 <sharmar19864@gmail.com>
2025-09-20 18:32:08 +0530

    Updated app.js with latest changes

--- app.js
+++ app.js
@@ -32,24 +32,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://personal-finance-tracker-frontend-tau.vercel.app"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
